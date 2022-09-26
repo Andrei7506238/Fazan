2bfe91a701ef2b861a2646ed95554468dfdb3975
Andrei7506238 <andrei750238@gmail.com>
2022-09-26 22:35:04 +0300

    Added processAnyway option
    Adversary words can now bypass the checking.

--- Fazan/FazanCheatGame.cpp
+++ Fazan/FazanCheatGame.cpp
@@ -3,7 +3,8 @@
 void FazanCheatGame::fazanCheatNoDuplicated(FazanDataStructure* x) {
 	std::cout << "\n\nA fost activat modul FazanCheat (fara duplicate).";
 	std::cout << "\nIntrodu toate cuvintele adversarilor (si cuvantul tau din prima etapa a jocului) cu caracterul '-' in fata pentru a sterge cuvantul din lista de cuvinte acceptate.";
-	std::cout << "\nIntrodu cuvantul adversarului aflat inaintea ta fara '-' pentru a sugera un cuvant cu numar cat mai mic de posibilitati de continuare. Cuvantul adversarului si cuvantul sugerat de program sunt sterse automat. ";
+	std::cout << "\nIntrodu cuvantul adversarului aflat inaintea ta fara '-' pentru a sugera un cuvant cu numar cat mai mic de posibilitati de continuare.";
+	std::cout << "\nCuvantul adversarului si cuvantul sugerat de program sunt sterse automat. Pentru a sari peste verificarea cuvantului adversarului adauga * la finalul cuvantului";
 	std::cout << "\nIntrodu '0' pentru a opri jocul.\n";
 
 	std::string nw;
@@ -19,14 +20,25 @@ void FazanCheatGame::fazanCheatNoDuplicated(FazanDataStructure* x) {
 		bool onlyDelete = (nw[0] == '-');
 		if (onlyDelete)	nw = nw.substr(1);
 
-		//Check if word is valid
+		//Check if word is marked to be processed anyway, regardless if it's invalid
+		bool processAnyway = (nw[nw.size() - 1] == '*');
+		if (processAnyway) nw = nw.substr(0, nw.size() - 1);
+
+		//Check if the word is valid
+		bool wordIsValid = true;
 		if (!x->checkWordExists(nw)) {
-			std::cout << "\tCuvant invalid sau deja folosit";
-			continue;
+			wordIsValid = false;
+
+			//If process anyway skip over the message and continue round
+			if (!processAnyway) {
+				std::cout << "\tCuvant invalid sau deja folosit";
+				continue;
+			}
 		}
 
-		//Delete adversary word
-		x->deleteWord(nw);
+		//Delete adversary word if word is valid
+		if(wordIsValid)
+			x->deleteWord(nw);
 
 		//If not marked only for deletion get suggestion
 		if (!onlyDelete) {
@@ -44,9 +56,11 @@ void FazanCheatGame::fazanCheatNoDuplicated(FazanDataStructure* x) {
 		}
 	}
 }
+
 void FazanCheatGame::fazanCheatDuplicated(FazanDataStructure* x) {
 	std::cout << "\n\nA fost activat modul FazanCheat (cu duplicate).";
 	std::cout << "\nIntrodu cuvantul adversarului aflat inaintea ta pentru a sugera un cuvant cu numar cat mai mic de posibilitati de continuare. Cuvantul adversarului si cuvantul sugerat de program sunt sterse automat. ";
+	std::cout << "\nIn mod implicit se va verifica daca cuvantul adversarului exista. Pentru a anula acest lucru adauga * dupa cuvant.";
 	std::cout << "\nIntrodu '0' pentru a opri jocul.\n";
 
 	std::string nw;
@@ -58,8 +72,12 @@ void FazanCheatGame::fazanCheatDuplicated(FazanDataStructure* x) {
 		//Check if game is over
 		if (nw == "0") break;
 
-		//Check if word is valid
-		if (!x->checkWordExists(nw)) {
+		//Check if word is marked to be processed anyway, regardless if it's invalid
+		bool processAnyway = (nw[nw.size() - 1] == '*');
+		if (processAnyway) nw = nw.substr(0, nw.size() - 1);
+
+		//If process anyway check if word is valid
+		if (!processAnyway && !x->checkWordExists(nw)) {
 			std::cout << "\tCuvant invalid sau deja folosit";
 			continue;
 		}
